aa622fd4e78834b0da0ffda7caa59b0df01ef7db
TakshPSingh <taksh001@gmail.com>
2020-07-05 14:52:31 +0530

    Added the ability for the server to connect to a database

---
+++ db/mongoose-connect.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useNewUrlParser', true)
+
+mongoose.connect(process.env.MONGODB_URL)
+
+module.exports = {mongoose};
