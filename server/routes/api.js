851b5e32d7eff3b10bc1e45ec51e5c760e4253ef
TakshPSingh <taksh001@gmail.com>
2020-07-14 01:32:27 +0530

    Added new router for handling calls

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,8 +4,10 @@ const router = express.Router()
 
 const donor = require('./donor')
 const patient = require('./patient')
+const call = require('./call')
 
 router.use('/donor', donor)
 router.use('/patient', patient)
+router.use('/call', call)
 
 module.exports = router
