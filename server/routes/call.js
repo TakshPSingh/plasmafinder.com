9a0acdc487c4b970d3ef66375d6029f4fe4b2f13
TakshPSingh <taksh001@gmail.com>
2020-07-19 00:57:36 +0530

    Added new route: /api/call/patient and wrote tests for it

--- server/routes/call.js
+++ server/routes/call.js
@@ -6,8 +6,10 @@ const router = express.Router()
 const {authenticateCall} = require('../middleware/authenticate_call')
 
 const {handleCallRequestFromDonor} = require('../../services/call/call_from_donor')
+const {handleCallRequestFromPatient} = require('../../services/call/call_from_patient')
 
 const {MAX_DURATION_OF_CALL_BY_DONOR} = require('../../constants/calls')
+const {MAX_DURATION_OF_CALL_BY_PATIENT} = require('../../constants/calls')
 
 router.get('/donor/:apiKey', authenticateCall, async (request, response) => {
     var body = _.pick(request.body, 'From')
@@ -19,6 +21,22 @@ router.get('/donor/:apiKey', authenticateCall, async (request, response) => {
                 numbers: [patientPhoneNumber]
             },
             max_conversation_duration: MAX_DURATION_OF_CALL_BY_DONOR
+        })
+    } catch (error) {
+        response.status(500).send()
+    }
+})
+
+router.get('/patient/:apiKey', authenticateCall, async (request, response) => {
+    var body = _.pick(request.body, 'From')
+
+    try {
+        var donorPhoneNumber = await handleCallRequestFromPatient(body.From)
+        response.status(200).send({
+            destination: {
+                numbers: [donorPhoneNumber]
+            },
+            max_conversation_duration: MAX_DURATION_OF_CALL_BY_PATIENT
         })
     } catch (error) {
         response.status(500).send()
---
+++ tests/routes/call/patient_to_donor.test.js
@@ -0,0 +1,91 @@
+const request = require('supertest')
+
+const {app} = require('../../../server/app')
+const {mongoose} = require('../../../db/mongoose_connect')
+
+const {Donor} = require('../../../models/donor')
+const {Patient} = require('../../../models/patient')
+
+const {handleDonorRegistration} = require('../../../services/donor/donor_registration')
+const {handlePatientRegistration} = require('../../../services/patient/patient_registration')
+const {verifyDonor} = require('../../../services/donor/donor_verification')
+const {verifyPatient} = require('../../../services/patient/patient_verification')
+const {matchPatientWithDonor} = require('../../../services/donation/match_patient_with_donor')
+
+const {MAX_NUMBER_OF_CALLS_ALLOWED_PER_DONATION_BY_PATIENT, MAX_DURATION_OF_CALL_BY_PATIENT} = require('../../../constants/calls')
+
+const DONOR_ONE_PHONE_NUMBER = '+919868711873'
+const PATIENT_ONE_PHONE_NUMBER = '+919716610606'
+
+beforeEach(async () => {
+    await Donor.deleteMany()
+    await Patient.deleteMany()
+})
+
+describe('Route: /api/call/patient', () => {
+    test('invalid api key', async () => {
+        await request(app)
+        .get('/api/call/patient/1234')
+        .send()
+        .expect(401)
+    })
+
+    test('invalid donor phone number', async () => {
+        await request(app)
+        .get(`/api/call/patient/${process.env.CALL_API_KEY}`)
+        .send()
+        .expect(200)
+        .then((response) => {
+            expect(response.body.destination.numbers).toHaveLength(1)
+            expect(response.body.destination.numbers[0]).toBeNull()
+            expect(response.body.max_conversation_duration).toBe(MAX_DURATION_OF_CALL_BY_PATIENT)
+        })
+    })
+
+    test('valid patient assigned to donor', async () => {
+        var patientInfo = {
+            name: 'Patient',
+            age: '22',
+            bloodGroup: 'A',
+            phone: PATIENT_ONE_PHONE_NUMBER,
+            email: 'abcd@gmail.com',
+            location: {
+                latitude: 0,
+                longitude: 0.00001
+            }
+        }
+        await handlePatientRegistration(patientInfo)
+        var patient = await Patient.findByPhoneNumber(PATIENT_ONE_PHONE_NUMBER)
+        await verifyPatient(PATIENT_ONE_PHONE_NUMBER, patient.otp)
+
+        var donorInfo = {
+            name: 'nhajd',
+            age: '22',
+            medicallyEligible: true,
+            bloodGroup: 'A',
+            phone: DONOR_ONE_PHONE_NUMBER,
+            email: 'abcdefgh@gmail.com',
+            location: {
+                latitude: 0,
+                longitude: 0
+            }
+        }
+        await handleDonorRegistration(donorInfo)
+        var donor = await Donor.findByPhoneNumber(DONOR_ONE_PHONE_NUMBER)
+        await verifyDonor(DONOR_ONE_PHONE_NUMBER, donor.otp)
+
+        var {patient, assignedDonor} = await matchPatientWithDonor(patient)
+
+        await request(app)
+        .get(`/api/call/patient/${process.env.CALL_API_KEY}`)
+        .send({
+            From: patient.phone
+        })
+        .expect(200)
+        .then((response) => {
+            expect(response.body.destination.numbers).toHaveLength(1)
+            expect(response.body.destination.numbers[0]).toBe(donor.phone)
+            expect(response.body.max_conversation_duration).toBe(MAX_DURATION_OF_CALL_BY_PATIENT)
+        })
+    })
+})
