19e59d529960eadcbd8a23c685f8ccebd370e0c8
TakshPSingh <taksh001@gmail.com>
2020-07-24 16:27:33 +0530

    Enabled cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 
 const api = require('./routes/api')
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/api', api)
 
